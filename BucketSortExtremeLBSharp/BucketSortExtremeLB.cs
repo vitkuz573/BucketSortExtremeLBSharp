8e9cde3fbc0d0c2915bb42f33764adbe15d34192
Vitaly Kuzyaev <vitkuz573@gmail.com>
2023-06-06 12:25:08 +0500

    Добавьте файлы проекта.

---
+++ BucketSortExtremeLBSharp/BucketSortExtremeLB.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BucketSortExtremeLBSharp;
+
+public class BucketSort
+{
+    private readonly double A;
+    private readonly double B;
+    private readonly double C;
+
+    public BucketSort(double A, double B, double C)
+    {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+    }
+
+    public double F(double x)
+    {
+        return Math.Exp(-Math.Pow((x - A) / B, -C));
+    }
+
+    public double FInverse(double u)
+    {
+        return A + B * Math.Pow(-Math.Log(u), -1 / C);
+    }
+
+    public List<double> Sort(List<double> input, bool descending)
+    {
+        var n = input.Count;
+
+        var buckets = new List<List<double>>(n);
+        for (int i = 0; i < n; i++)
+        {
+            buckets.Add(new List<double>());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int bucketIndex = (int)(F(input[i]) * n);
+
+            if (bucketIndex == n)
+            {
+                bucketIndex--;
+            }
+
+            buckets[bucketIndex].Add(input[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            buckets[i]?.Sort();
+            if (descending)
+            {
+                buckets[i]?.Reverse();
+            }
+        }
+
+        var sortedList = new List<double>();
+
+        if (descending)
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (buckets[i] != null)
+                {
+                    sortedList.AddRange(buckets[i]);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (buckets[i] != null)
+                {
+                    sortedList.AddRange(buckets[i]);
+                }
+            }
+        }
+
+        return sortedList;
+    }
+}
---
+++ BucketSortExtremeLBSharp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace BucketSortExtremeLBSharp;
+
+public partial class MainWindow : Window
+{
+    private BucketSort bucketSort;
+    private Random random;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        random = new Random();
+    }
+
+    private void GenerateButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            double A = Convert.ToDouble(ATextBox.Text);
+            double B = Convert.ToDouble(BTextBox.Text);
+            double C = Convert.ToDouble(CTextBox.Text);
+
+            bucketSort = new BucketSort(A, B, C);
+
+            int size = Convert.ToInt32(SizeTextBox.Text);
+            double minRange = Convert.ToDouble(MinRangeTextBox.Text);
+            double maxRange = Convert.ToDouble(MaxRangeTextBox.Text);
+
+            List<double> numbers = new List<double>();
+            for (int i = 0; i < size; i++)
+            {
+                numbers.Add(Math.Round(minRange + (random.NextDouble() * (maxRange - minRange))));
+            }
+
+            InputTextBox.Text = string.Join(",", numbers);
+        }
+        catch (Exception ex)
+        {
+            OutputTextBox.Text = $"Error: {ex.Message}";
+        }
+    }
+
+    private void SortButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            List<double> input = new List<double>(Array.ConvertAll(InputTextBox.Text.Split(','), double.Parse));
+            bool descending = DescendingCheckBox.IsChecked ?? false;
+            List<double> sortedList = bucketSort.Sort(input, descending);
+            OutputTextBox.Text = string.Join(",", sortedList);
+        }
+        catch (Exception ex)
+        {
+            OutputTextBox.Text = $"Error: {ex.Message}";
+        }
+    }
+}
